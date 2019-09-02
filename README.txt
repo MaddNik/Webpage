dc843604e01dae49831db43de04c2ba258ccf7a1
MaddNik <sainikshipthmaddhugaru@gmail.com>
2019-09-02 17:39:15 +0530

    first commit

---
+++ README.txt
@@ -0,0 +1,1 @@
+This is a static webpage designed in my free time, any suggestion appreciated
---
+++ assets/js/main.js
@@ -0,0 +1,640 @@
+/*
+	Dimension by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body'),
+		$wrapper = $('#wrapper'),
+		$header = $('#header'),
+		$footer = $('#footer'),
+		$main = $('#main'),
+		$main_articles = $main.children('article');
+
+	// Breakpoints.
+		breakpoints({
+			xlarge:   [ '1281px',  '1680px' ],
+			large:    [ '981px',   '1280px' ],
+			medium:   [ '737px',   '980px'  ],
+			small:    [ '481px',   '736px'  ],
+			xsmall:   [ '361px',   '480px'  ],
+			xxsmall:  [ null,      '360px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Fix: Flexbox min-height bug on IE.
+		if (browser.name == 'ie') {
+
+			var flexboxFixTimeoutId;
+
+			$window.on('resize.flexbox-fix', function() {
+
+				clearTimeout(flexboxFixTimeoutId);
+
+				flexboxFixTimeoutId = setTimeout(function() {
+
+					if ($wrapper.prop('scrollHeight') > $window.height())
+						$wrapper.css('height', 'auto');
+					else
+						$wrapper.css('height', '100vh');
+
+				}, 250);
+
+			}).triggerHandler('resize.flexbox-fix');
+
+		}
+
+	// Nav.
+		var $nav = $header.children('nav'),
+			$nav_li = $nav.find('li');
+
+		// Add "middle" alignment classes if we're dealing with an even number of items.
+			if ($nav_li.length % 2 == 0) {
+
+				$nav.addClass('use-middle');
+				$nav_li.eq( ($nav_li.length / 2) ).addClass('is-middle');
+
+			}
+
+	// Main.
+		var	delay = 325,
+			locked = false;
+
+		// Methods.
+			$main._show = function(id, initial) {
+
+				var $article = $main_articles.filter('#' + id);
+
+				// No such article? Bail.
+					if ($article.length == 0)
+						return;
+
+				// Handle lock.
+
+					// Already locked? Speed through "show" steps w/o delays.
+						if (locked || (typeof initial != 'undefined' && initial === true)) {
+
+							// Mark as switching.
+								$body.addClass('is-switching');
+
+							// Mark as visible.
+								$body.addClass('is-article-visible');
+
+							// Deactivate all articles (just in case one's already active).
+								$main_articles.removeClass('active');
+
+							// Hide header, footer.
+								$header.hide();
+								$footer.hide();
+
+							// Show main, article.
+								$main.show();
+								$article.show();
+
+							// Activate article.
+								$article.addClass('active');
+
+							// Unlock.
+								locked = false;
+
+							// Unmark as switching.
+								setTimeout(function() {
+									$body.removeClass('is-switching');
+								}, (initial ? 1000 : 0));
+
+							return;
+
+						}
+
+					// Lock.
+						locked = true;
+
+				// Article already visible? Just swap articles.
+					if ($body.hasClass('is-article-visible')) {
+
+						// Deactivate current article.
+							var $currentArticle = $main_articles.filter('.active');
+
+							$currentArticle.removeClass('active');
+
+						// Show article.
+							setTimeout(function() {
+
+								// Hide current article.
+									$currentArticle.hide();
+
+								// Show article.
+									$article.show();
+
+								// Activate article.
+									setTimeout(function() {
+
+										$article.addClass('active');
+
+										// Window stuff.
+											$window
+												.scrollTop(0)
+												.triggerHandler('resize.flexbox-fix');
+
+										// Unlock.
+											setTimeout(function() {
+												locked = false;
+											}, delay);
+
+									}, 25);
+
+							}, delay);
+
+					}
+
+				// Otherwise, handle as normal.
+					else {
+
+						// Mark as visible.
+							$body
+								.addClass('is-article-visible');
+
+						// Show article.
+							setTimeout(function() {
+
+								// Hide header, footer.
+									$header.hide();
+									$footer.hide();
+
+								// Show main, article.
+									$main.show();
+									$article.show();
+
+								// Activate article.
+									setTimeout(function() {
+
+										$article.addClass('active');
+
+										// Window stuff.
+											$window
+												.scrollTop(0)
+												.triggerHandler('resize.flexbox-fix');
+
+										// Unlock.
+											setTimeout(function() {
+												locked = false;
+											}, delay);
+
+									}, 25);
+
+							}, delay);
+
+					}
+
+			};
+
+			$main._hide = function(addState) {
+
+				var $article = $main_articles.filter('.active');
+
+				// Article not visible? Bail.
+					if (!$body.hasClass('is-article-visible'))
+						return;
+
+				// Add state?
+					if (typeof addState != 'undefined'
+					&&	addState === true)
+						history.pushState(null, null, '#');
+
+				// Handle lock.
+
+					// Already locked? Speed through "hide" steps w/o delays.
+						if (locked) {
+
+							// Mark as switching.
+								$body.addClass('is-switching');
+
+							// Deactivate article.
+								$article.removeClass('active');
+
+							// Hide article, main.
+								$article.hide();
+								$main.hide();
+
+							// Show footer, header.
+								$footer.show();
+								$header.show();
+
+							// Unmark as visible.
+								$body.removeClass('is-article-visible');
+
+							// Unlock.
+								locked = false;
+
+							// Unmark as switching.
+								$body.removeClass('is-switching');
+
+							// Window stuff.
+								$window
+									.scrollTop(0)
+									.triggerHandler('resize.flexbox-fix');
+
+							return;
+
+						}
+
+					// Lock.
+						locked = true;
+
+				// Deactivate article.
+					$article.removeClass('active');
+
+				// Hide article.
+					setTimeout(function() {
+
+						// Hide article, main.
+							$article.hide();
+							$main.hide();
+
+						// Show footer, header.
+							$footer.show();
+							$header.show();
+
+						// Unmark as visible.
+							setTimeout(function() {
+
+								$body.removeClass('is-article-visible');
+
+								// Window stuff.
+									$window
+										.scrollTop(0)
+										.triggerHandler('resize.flexbox-fix');
+
+								// Unlock.
+									setTimeout(function() {
+										locked = false;
+									}, delay);
+
+							}, 25);
+
+					}, delay);
+
+
+			};
+
+		// Articles.
+			$main_articles.each(function() {
+
+				var $this = $(this);
+
+				// Close.
+					$('<div class="close">Close</div>')
+						.appendTo($this)
+						.on('click', function() {
+							location.hash = '';
+						});
+
+				// Prevent clicks from inside article from bubbling.
+					$this.on('click', function(event) {
+						event.stopPropagation();
+					});
+
+			});
+
+		// Events.
+			$body.on('click', function(event) {
+
+				// Article visible? Hide.
+					if ($body.hasClass('is-article-visible'))
+						$main._hide(true);
+
+			});
+
+			$window.on('keyup', function(event) {
+
+				switch (event.keyCode) {
+
+					case 27:
+
+						// Article visible? Hide.
+							if ($body.hasClass('is-article-visible'))
+								$main._hide(true);
+
+						break;
+
+					default:
+						break;
+
+				}
+
+			});
+
+			$window.on('hashchange', function(event) {
+
+				// Empty hash?
+					if (location.hash == ''
+					||	location.hash == '#') {
+
+						// Prevent default.
+							event.preventDefault();
+							event.stopPropagation();
+
+						// Hide.
+							$main._hide();
+
+					}
+
+				// Otherwise, check for a matching article.
+					else if ($main_articles.filter(location.hash).length > 0) {
+
+						// Prevent default.
+							event.preventDefault();
+							event.stopPropagation();
+
+						// Show article.
+							$main._show(location.hash.substr(1));
+
+					}
+
+			});
+
+		// Scroll restoration.
+		// This prevents the page from scrolling back to the top on a hashchange.
+			if ('scrollRestoration' in history)
+				history.scrollRestoration = 'manual';
+			else {
+
+				var	oldScrollPos = 0,
+					scrollPos = 0,
+					$htmlbody = $('html,body');
+
+				$window
+					.on('scroll', function() {
+
+						oldScrollPos = scrollPos;
+						scrollPos = $htmlbody.scrollTop();
+
+					})
+					.on('hashchange', function() {
+						$window.scrollTop(oldScrollPos);
+					});
+
+			}
+
+		// Initialize.
+
+			// Hide main, articles.
+				$main.hide();
+				$main_articles.hide();
+
+			// Initial article.
+				if (location.hash != ''
+				&&	location.hash != '#')
+					$window.on('load', function() {
+						$main._show(location.hash.substr(1), true);
+					});
+
+})(jQuery);
+
+
+
+
+
+
+
+
+
+function WordShuffler(holder,opt){
+  var that = this;
+  var time = 0;
+  this.now;
+  this.then = Date.now();
+  
+  this.delta;
+  this.currentTimeOffset = 0;
+  
+  this.word = null;
+  this.currentWord = null;
+  this.currentCharacter = 0;
+  this.currentWordLength = 0;
+
+
+  var options = {
+    fps : 20,
+    timeOffset : 5,
+    textColor : '#000',
+    fontSize : "50px",
+    useCanvas : false,
+    mixCapital : false,
+    mixSpecialCharacters : false,
+    needUpdate : true,
+    colors : [
+      '#fff'
+    ]
+  }
+
+  if(typeof opt != "undefined"){
+    for(key in opt){
+      options[key] = opt[key];
+    }
+  }
+
+
+  
+  this.needUpdate = true;
+  this.fps = options.fps;
+  this.interval = 1000/this.fps;
+  this.timeOffset = options.timeOffset;
+  this.textColor = options.textColor;
+  this.fontSize = options.fontSize;
+  this.mixCapital = options.mixCapital;
+  this.mixSpecialCharacters = options.mixSpecialCharacters;
+  this.colors = options.colors;
+
+   this.useCanvas = options.useCanvas;
+  
+  this.chars = [
+    'A','B','C','D',
+    'E','F','G','H',
+    'I','J','K','L',
+    'M','N','O','P',
+    'Q','R','S','T',
+    'U','V','W','X',
+    'Y','Z'
+  ];
+  this.specialCharacters = [
+    '!','§','$','%',
+    '&','/','(',')',
+    '=','?','_','<',
+    '>','^','°','*',
+    '#','-',':',';','~'
+  ]
+
+  if(this.mixSpecialCharacters){
+    this.chars = this.chars.concat(this.specialCharacters);
+  }
+
+  this.getRandomColor = function () {
+    var randNum = Math.floor( Math.random() * this.colors.length );
+    return this.colors[randNum];
+  }
+
+  //if Canvas
+ 
+  this.position = {
+    x : 0,
+    y : 50
+  }
+
+  //if DOM
+  if(typeof holder != "undefined"){
+    this.holder = holder;
+  }
+
+  if(!this.useCanvas && typeof this.holder == "undefined"){
+    console.warn('Holder must be defined in DOM Mode. Use Canvas or define Holder');
+  }
+
+
+  this.getRandCharacter = function(characterToReplace){    
+    if(characterToReplace == " "){
+      return ' ';
+    }
+    var randNum = Math.floor(Math.random() * this.chars.length);
+    var lowChoice =  -.5 + Math.random();
+    var picketCharacter = this.chars[randNum];
+    var choosen = picketCharacter.toLowerCase();
+    if(this.mixCapital){
+      choosen = lowChoice < 0 ? picketCharacter.toLowerCase() : picketCharacter;
+    }
+    return choosen;
+    
+  }
+
+  this.writeWord = function(word){
+    this.word = word;
+    this.currentWord = word.split('');
+    this.currentWordLength = this.currentWord.length;
+
+  }
+
+  this.generateSingleCharacter = function (color,character) {
+    var span = document.createElement('span');
+    span.style.color = color;
+    span.innerHTML = character;
+    return span;
+  }
+
+  this.updateCharacter = function (time) {
+    
+      this.now = Date.now();
+      this.delta = this.now - this.then;
+
+       
+
+      if (this.delta > this.interval) {
+        this.currentTimeOffset++;
+      
+        var word = [];
+
+        if(this.currentTimeOffset === this.timeOffset && this.currentCharacter !== this.currentWordLength){
+          this.currentCharacter++;
+          this.currentTimeOffset = 0;
+        }
+        for(var k=0;k<this.currentCharacter;k++){
+          word.push(this.currentWord[k]);
+        }
+
+        for(var i=0;i<this.currentWordLength - this.currentCharacter;i++){
+          word.push(this.getRandCharacter(this.currentWord[this.currentCharacter+i]));
+        }
+
+
+        if(that.useCanvas){
+          c.clearRect(0,0,stage.x * stage.dpr , stage.y * stage.dpr);
+          c.font = that.fontSize + " sans-serif";
+          var spacing = 0;
+          word.forEach(function (w,index) {
+            if(index > that.currentCharacter){
+              c.fillStyle = that.getRandomColor();
+            }else{
+              c.fillStyle = that.textColor;
+            }
+            c.fillText(w, that.position.x + spacing, that.position.y);
+            spacing += c.measureText(w).width;
+          });
+        }else{
+
+          if(that.currentCharacter === that.currentWordLength){
+            that.needUpdate = false;
+          }
+          this.holder.innerHTML = '';
+          word.forEach(function (w,index) {
+            var color = null
+            if(index > that.currentCharacter){
+              color = that.getRandomColor();
+            }else{
+              color = that.textColor;
+            }
+            that.holder.appendChild(that.generateSingleCharacter(color, w));
+          }); 
+        }
+        this.then = this.now - (this.delta % this.interval);
+      }
+  }
+
+  this.restart = function () {
+    this.currentCharacter = 0;
+    this.needUpdate = true;
+  }
+
+  function update(time) {
+    time++;
+    if(that.needUpdate){
+      that.updateCharacter(time);
+    }
+    requestAnimationFrame(update);
+  }
+
+  this.writeWord(this.holder.innerHTML);
+
+
+  console.log(this.currentWord);
+  update(time);
+}
+
+
+
+
+var headline = document.getElementById('headline');
+var text = document.getElementById('text');
+var shuffler = document.getElementById('shuffler');
+
+var headText = new WordShuffler(headline,{
+  textColor : '#fff',
+  timeOffset : 3,/* animation timer*/
+  mixCapital : true,
+  mixSpecialCharacters : true
+});
+
+var pText = new WordShuffler(text,{
+  textColor : '#fff',
+  timeOffset : 2
+});
+
+var buttonText = new WordShuffler(shuffler,{
+  textColor : 'tomato',
+  timeOffset : 10
+});
+
+
+
+  shuffler.addEventListener('click',function () {
+    headText.restart();
+    pText.restart();
+    buttonText.restart();
+  });
